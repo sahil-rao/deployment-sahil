d0a0ffffd7e8a9ba2a22da5652e605ba8dbefb21
Ubuntu <ubuntu@ip-10-0-0-165.us-west-1.compute.internal>
2014-02-25 17:22:15 +0000

    Modified refresh script to start node.

--- VM/refresh.sh
+++ VM/refresh.sh
@@ -1,6 +1,7 @@
 #Restarts running services 
 sudo mongod --fork --logpath /mnt/volume1/mongo/log/mongo.log -dbpath /mnt/volume1/mongo/db 
 sudo service baazdataacquisition restart                               
-sudo service baazcompile restart                                   
-sudo service apache2 restart  
+sudo service baazcompile restart  
+sudo stop nodejs                                 
+sudo start nodejs                                 
 exit 0
