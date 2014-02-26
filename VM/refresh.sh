ace36397164704ebf986258db6f6ffc0e23ed239
Ubuntu <ubuntu@ip-10-0-0-165.us-west-1.compute.internal>
2014-02-26 15:27:10 +0000

    Changed the owner for mongo files to mongo db. Use upstart to start mongdb service. Note for already running servers this still needs to be done manually.

--- VM/refresh.sh
+++ VM/refresh.sh
@@ -3,5 +3,6 @@ sudo mongod --fork --logpath /mnt/volume1/mongo/log/mongo.log -dbpath /mnt/volum
 sudo service baazdataacquisition restart                               
 sudo service baazcompile restart  
 sudo stop nodejs                                 
-sudo start nodejs                                 
+sudo start nodejs  
+sudo monit                               
 exit 0
