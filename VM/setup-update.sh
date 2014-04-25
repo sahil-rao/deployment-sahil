55670419a685e8b676ea5946d70f06ac03d113dc
Ubuntu <ubuntu@ip-10-0-0-165.us-west-1.compute.internal>
2014-04-25 14:51:32 -0700

    - Updated the user account in VM to be xplain. - Fixes in VM deployment - Added bto to be installed in S3 deployment.

--- VM/setup-update.sh
+++ VM/setup-update.sh
@@ -1,8 +1,8 @@
 #Sets up Ansible, Runs Ansible
 echo "Installling Hive"
 sudo rm -rf /usr/lib/hive
-sudo tar -zxf /home/ubuntu/apache-hive-0.13.0-SNAPSHOT-bin.tar.gz -C /home/ubuntu
-sudo mv /home/ubuntu/apache-hive-0.13.0-SNAPSHOT-bin /usr/lib/hive
+sudo tar -zxf /home/xplain/apache-hive-0.13.0-SNAPSHOT-bin.tar.gz -C /home/xplain
+sudo mv /home/xplain/apache-hive-0.13.0-SNAPSHOT-bin /usr/lib/hive
 echo "Running playbook vm-update.yml"
 sudo ansible-playbook vm-update.yml --connection=local
 sudo ./refresh.sh
