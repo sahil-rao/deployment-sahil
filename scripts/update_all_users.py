f0c027eb4c3831c8ef961680f60f42b27d454d2a
Samir Pujari <samir@baazdata.com>
2014-12-05 10:52:12 -0800

    Updating script

--- scripts/update_all_users.py
+++ scripts/update_all_users.py
@@ -10,19 +10,8 @@ def run_workflow(mongo_url):
         if "data_files" not in mongo_client[db].collection_names():
             continue
 
-        if mongo_client[db].data_files.find_one({"dashboard_topbar_v4.file" : "dashboard_topbar_v4_demo.hbs"}) is not None:
+        if mongo_client[db].data_files.find_one({}) is not None:
             print "Upgrading " + db
-            mongo_client[db].data_files.update({'dashboard_v4.layout': 'dashboard_layout_v4'},
-                                                 { "dashboard_v4": { 
-                                                        "layout": "dashboard_layout_v4", 
-                                                        "components": [ 
-                                                            { "topbar": "dashboard_topbar_v4" },
-                                                            { "insights": "insights_v2" },
-                                                            { "entity_rel_diagram_dashboard": "entity_rel_diagram_dashboard_v1" },
-                                                            { "discover_design_transform": "discover_design_transform_v2" },
-                                                            { "upload_modal": "upload_modal_v2" }, 
-                                                            { "component_scripts": "dashboard_scripts_v4" }, 
-                                                            { "component_stylesheets": "dashboard_stylesheets_v4" }], 
-                                                        "template_engine_version": "1"}})
+            mongo_client[db].data_files.update({'dashboard_v4.layout': 'dashboard_layout_v4'},{ "dashboard_v4": {"layout": "dashboard_layout_v4","components": [{ "topbar": "dashboard_topbar_v4" },{ "insights": "insights_v2" },{ "entity_rel_diagram_dashboard": "entity_rel_diagram_dashboard_v1" },{ "discover_design_transform": "discover_design_transform_v2" },{ "upload_modal": "upload_modal_v2" },{ "component_scripts": "dashboard_scripts_v4" },{ "component_stylesheets": "dashboard_stylesheets_v4" }],"template_engine_version": "1"}})
 
-#run_workflow('mongodb://127.0.0.1')
+#run_workflow('mongodb://10.0.0.231')
