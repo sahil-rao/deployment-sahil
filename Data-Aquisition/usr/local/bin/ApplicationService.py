496772a2a6c41d4c7ede2bfc4cc7e9e05116621d
ravi shetye <ravi@xplain.io>
2016-01-05 16:14:19 -0800

    NAVOPT-526 : Delete User does not work

--- Data-Aquisition/usr/local/bin/ApplicationService.py
+++ Data-Aquisition/usr/local/bin/ApplicationService.py
@@ -432,8 +432,6 @@ def callback(ch, method, properties, body):
             resp_dict = {'redis_master_name': FPConnector.get_redis_master_name(tenant)}
         elif msg_dict['opcode'] == "ElasticNodesForTenant":
             resp_dict = {'elastic_nodes': FPConnector.get_elastic_nodes(tenant)}
-        elif msg_dict['opcode'] == "CleanupUser":
-            resp_dict = cleanup_user.execute(tenant)
         else:
             api_config= ConfigParser.RawConfigParser()
             api_config.read("/etc/xplain/application-api.cfg")
