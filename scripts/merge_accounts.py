dad9c25b3d57030edd5529dbf095cca9c434dae6
Samir Pujari <samir@baazdata.com>
2016-02-05 10:50:07 -0800

    Adding script to merge accounts

---
+++ scripts/merge_accounts.py
@@ -0,0 +1,33 @@
+# (c) Copyright 2015 Cloudera, Inc. All rights reserved.
+
+"""
+Script to merge two user accounts.
+"""
+
+import sys
+import csv
+import pprint
+from flightpath.Provenance import getMongoServer
+
+
+def execute(params):
+    '''
+    Returns information about all users in the system.
+    '''
+    client = getMongoServer('xplainIO')
+
+    db = client['xplainIO']
+    user = db.users.find_one({'email': msg_dict['merge_into_email']})
+    if user:
+        db.organizations.update({'guid': msg_dict['old_tenantid']},
+                                {'$set': {'users': [msg_dict['merge_into_email']]}})
+        db.users.update({'email': msg_dict['merge_into_email']},
+                        {'$push': {'organizations': msg_dict['old_tenantid']}})
+        db.users.remove({'email': msg_dict['old_email']})
+    else:
+        print msg_dict
+
+if __name__ == '__main__':
+    params = []
+    for msg_dict in params:
+        execute(msg_dict)
