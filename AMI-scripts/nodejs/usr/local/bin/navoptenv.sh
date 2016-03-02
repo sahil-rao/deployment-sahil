57f70a676fd608d6e8fc0878ce63833bac55a0aa
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-03-01 18:43:00 -0800

    NodeJS AMI scripts

---
+++ AMI-scripts/nodejs/usr/local/bin/navoptenv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# buildpath: path to UI tarball build ( xplain.io.tar.gz)
+# configpath: path to UI config json
+
+userdata=`curl --silent http://169.254.169.254/latest/user-data`
+grep="grep"
+regex='s/.*\:[ \t]*"\{0,1\}\([^,"]*\)"\{0,1\},\{0,1\}/\1/'
+sed="sed '${regex}'"
+
+
+
+# if [ "${userdata}" != "" ]; then
+# 	# basic settings
+# 	export BUILD_S3PATH=`eval "echo '${userdata}' | ${grep} '\"buildpath\"' | ${sed}"`
+# 	export CONFIG_S3PATH=`eval "echo '${userdata}' | ${grep} '\"configpath\"' | ${sed}"`
+# fi
+
+export EC2_AVAILABILITY_ZONE=`curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone`
+export AWS_DEFAULT_REGION="`echo \"$EC2_AVAILABILITY_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+export EC2_INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
+
+IAM_ROLE=`curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/`
+export AWSAccessKeyId=`curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${IAM_ROLE} | jq -r '.AccessKeyId'`
+export AWSSecretAccessKey=`curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${IAM_ROLE} | jq -r '.SecretAccessKey'`
+
+export BUILD_S3PATH="s3://baaz-deployment/xplain.io.tar.gz"
+export CONFIG_S3PATH="s3://baaz-deployment/config.json"
+
