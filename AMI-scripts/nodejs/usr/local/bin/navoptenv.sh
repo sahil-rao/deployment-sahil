de5c34c91700d73ebdd9a4f9a4943325f5951874
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-03-01 18:45:28 -0800

    Dont need AWS keys because we are pulling from IAM roles

--- AMI-scripts/nodejs/usr/local/bin/navoptenv.sh
+++ AMI-scripts/nodejs/usr/local/bin/navoptenv.sh
@@ -19,11 +19,6 @@ sed="sed '${regex}'"
 export EC2_AVAILABILITY_ZONE=`curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone`
 export AWS_DEFAULT_REGION="`echo \"$EC2_AVAILABILITY_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 export EC2_INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
-
-IAM_ROLE=`curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/`
-export AWSAccessKeyId=`curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${IAM_ROLE} | jq -r '.AccessKeyId'`
-export AWSSecretAccessKey=`curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${IAM_ROLE} | jq -r '.SecretAccessKey'`
-
 export BUILD_S3PATH="s3://baaz-deployment/xplain.io.tar.gz"
 export CONFIG_S3PATH="s3://baaz-deployment/config.json"
 
