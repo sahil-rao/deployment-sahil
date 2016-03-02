d27ef509aebb77100b02563f26125eb529d6b8bd
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-03-01 18:47:18 -0800

    Pull from instance userdata instead of hardcoded

--- AMI-scripts/nodejs/usr/local/bin/navoptenv.sh
+++ AMI-scripts/nodejs/usr/local/bin/navoptenv.sh
@@ -10,15 +10,13 @@ sed="sed '${regex}'"
 
 
 
-# if [ "${userdata}" != "" ]; then
-# 	# basic settings
-# 	export BUILD_S3PATH=`eval "echo '${userdata}' | ${grep} '\"buildpath\"' | ${sed}"`
-# 	export CONFIG_S3PATH=`eval "echo '${userdata}' | ${grep} '\"configpath\"' | ${sed}"`
-# fi
+if [ "${userdata}" != "" ]; then
+	export BUILD_S3PATH=`eval "echo '${userdata}' | ${grep} '\"buildpath\"' | ${sed}"`
+	export CONFIG_S3PATH=`eval "echo '${userdata}' | ${grep} '\"configpath\"' | ${sed}"`
+fi
 
 export EC2_AVAILABILITY_ZONE=`curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone`
 export AWS_DEFAULT_REGION="`echo \"$EC2_AVAILABILITY_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 export EC2_INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
-export BUILD_S3PATH="s3://baaz-deployment/xplain.io.tar.gz"
-export CONFIG_S3PATH="s3://baaz-deployment/config.json"
+
 
