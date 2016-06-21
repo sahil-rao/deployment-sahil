3b9e9f136f3c859169b4f6693673995bd41fc9c7
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-06-20 17:22:47 -0700

    Terraforming scripts for alpha on mongodb redis

---
+++ terraform/mongodb/mongodb.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    access_key = "AKIAJ7LQXN75F7K25JIQ"
+    secret_key = "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"
+    region = "us-west-1"
+}
+resource "aws_launch_configuration" "mongodb_cluster_lc" {
+    image_id = "ami-08b7f268"
+    instance_type = "m4.xlarge"
+    iam_instance_profile = "MongoDB_Server"
+    ebs_optimized = true
+    enable_monitoring = false
+    key_name = "Baaz-Deployment"
+    security_groups = ["sg-acd8d1ce"]
+    user_data = "{\"dbsilo\": \"dbsilo4\", \"service\": \"mongo\", \"cluster\": \"alpha\", \"datadog_api_key\": \"42bbac658841fd4c44253c01423b3227\"}"
+}
+resource "aws_autoscaling_group" "mongodb_cluster_asg" {
+    max_size = 3
+    min_size = 1
+    launch_configuration = "${aws_launch_configuration.mongodb_cluster_lc.name}"
+    desired_capacity = 1
+    vpc_zone_identifier = ["subnet-6caca20e", "subnet-82145ec4"]
+}
---
+++ terraform/redis/redis.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    access_key = "AKIAJ7LQXN75F7K25JIQ"
+    secret_key = "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"
+    region = "us-west-1"
+}
+resource "aws_launch_configuration" "redis_cluster_lc" {
+    image_id = "ami-5e195d3e"
+    instance_type = "r3.2xlarge"
+    iam_instance_profile = "MongoDB_Server"
+    ebs_optimized = true
+    enable_monitoring = false
+    key_name = "Baaz-Deployment"
+    security_groups = ["sg-a8b7b5cd"]
+    user_data = "{\"dbsilo\": \"dbsilo4\", \"service\": \"redis\", \"cluster\": \"alpha\", \"datadog_api_key\": \"42bbac658841fd4c44253c01423b3227\"}"
+}
+resource "aws_autoscaling_group" "redis_cluster_asg" {
+    max_size = 3
+    min_size = 1
+    launch_configuration = "${aws_launch_configuration.redis_cluster_lc.name}"
+    desired_capacity = 1
+    vpc_zone_identifier = ["subnet-6caca20e", "subnet-82145ec4"]
+}
