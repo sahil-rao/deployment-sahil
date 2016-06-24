2ce48a0e83b9e9b05867714e366771e0eb2df7d0
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-06-24 14:29:35 -0700

    New state file

--- terraform/accounts/stage-us-west-1/main.tf
+++ terraform/accounts/stage-us-west-1/main.tf
@@ -67,23 +67,23 @@ module "dbsilo4" {
     cluster_name = "${var.cluster_name}"
     datadog_api_key = "${var.datadog_api_key}"
 
-    mongo_blue_ami_id = "ami-50400430"
+    mongo_blue_ami_id = "ami-a699ddc6"
     mongo_blue_instance_type = "m4.xlarge"
-    mongo_blue_min_size = 1
-    mongo_blue_max_size = 3
-    mongo_blue_desired_capacity = 3
+    mongo_blue_min_size = 0
+    mongo_blue_max_size = 0
+    mongo_blue_desired_capacity = 0
 
-    mongo_green_ami_id = "ami-50400430"
+    mongo_green_ami_id = "ami-a699ddc6"
     mongo_green_instance_type = "m4.xlarge"
     mongo_green_min_size = 1
     mongo_green_max_size = 3
-    mongo_green_desired_capacity = 3
+    mongo_green_desired_capacity = 1
 
     redis_blue_ami_id = "ami-64400404"
     redis_blue_instance_type = "r3.2xlarge"
     redis_blue_min_size = 1
     redis_blue_max_size = 3
-    redis_blue_desired_capacity = 1
+    redis_blue_desired_capacity = 3
 
     redis_green_ami_id = "ami-64400404"
     redis_green_instance_type = "r3.2xlarge"
