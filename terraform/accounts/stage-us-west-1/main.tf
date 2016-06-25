2005effb80af1f2c1a05d2a67f5ecb5710cffb1d
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-06-24 18:45:24 -0700

    Update amis

--- terraform/accounts/stage-us-west-1/main.tf
+++ terraform/accounts/stage-us-west-1/main.tf
@@ -80,25 +80,25 @@ module "dbsilo2" {
    mongo_green_max_size = 0
    mongo_green_desired_capacity = 0
 
-   redis_blue_ami_id = "ami-319bdf51"
+   redis_blue_ami_id = "ami-cfa0e4af"
    redis_blue_instance_type = "r3.2xlarge"
    redis_blue_min_size = 0
    redis_blue_max_size = 0
    redis_blue_desired_capacity = 0
 
-   redis_green_ami_id = "ami-319bdf51"
+   redis_green_ami_id = "ami-cfa0e4af"
    redis_green_instance_type = "r3.2xlarge"
    redis_green_min_size = 0
    redis_green_max_size = 0
    redis_green_desired_capacity = 0
 
-   elasticsearch_blue_ami_id = "ami-bb9bdfdb"
+   elasticsearch_blue_ami_id = "ami-01a7e361"
    elasticsearch_blue_instance_type = "m3.xlarge"
    elasticsearch_blue_min_size = 0
    elasticsearch_blue_max_size = 0
    elasticsearch_blue_desired_capacity = 0
 
-   elasticsearch_green_ami_id = "ami-bb9bdfdb"
+   elasticsearch_green_ami_id = "ami-01a7e361"
    elasticsearch_green_instance_type = "m3.xlarge"
    elasticsearch_green_min_size = 0
    elasticsearch_green_max_size = 0
@@ -119,13 +119,13 @@ module "dbsilo4" {
     cluster_name = "${var.cluster_name}"
     datadog_api_key = "${var.datadog_api_key}"
 
-    mongo_blue_ami_id = "ami-a699ddc6"
+    mongo_blue_ami_id = "ami-32a6e252"
     mongo_blue_instance_type = "m4.xlarge"
     mongo_blue_min_size = 1
     mongo_blue_max_size = 3
     mongo_blue_desired_capacity = 3
 
-    mongo_green_ami_id = "ami-a699ddc6"
+    mongo_green_ami_id = "ami-32a6e252"
     mongo_green_instance_type = "m4.xlarge"
     mongo_green_min_size = 1
     mongo_green_max_size = 3
@@ -138,20 +138,20 @@ module "dbsilo4" {
     redis_blue_desired_capacity = 3
     redis_blue_backup_file = "s3://xplain-alpha/redis-backups/${var.cluster_name}-dump.rdb"
 
-    redis_green_ami_id = "ami-179eda77"
+    redis_green_ami_id = "ami-cfa0e4af"
     redis_green_instance_type = "r3.2xlarge"
     redis_green_min_size = 1
     redis_green_max_size = 3
     redis_green_desired_capacity = 3
     redis_green_backup_file = ""
 
-    elasticsearch_blue_ami_id = "ami-bb9bdfdb"
+    elasticsearch_blue_ami_id = "ami-01a7e361"
     elasticsearch_blue_instance_type = "m3.xlarge"
     elasticsearch_blue_min_size = 0
     elasticsearch_blue_max_size = 0
     elasticsearch_blue_desired_capacity = 0
 
-    elasticsearch_green_ami_id = "ami-bb9bdfdb"
+    elasticsearch_green_ami_id = "ami-01a7e361"
     elasticsearch_green_instance_type = "m3.xlarge"
     elasticsearch_green_min_size = 3
     elasticsearch_green_max_size = 3
