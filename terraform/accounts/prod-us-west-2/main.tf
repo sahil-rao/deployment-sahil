f157b6e9d9b0723db52d5518e60fd1b7e872741b
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-06-27 13:42:02 -0700

    New state file

--- terraform/accounts/prod-us-west-2/main.tf
+++ terraform/accounts/prod-us-west-2/main.tf
@@ -20,9 +20,9 @@ module "dbsilo3" {
     mongo_blue_name_prefix = "${var.cluster_name}-dbsilo3-mongo-blue-v2"
     mongo_blue_ami_id = "ami-8537f0e5"
     mongo_blue_instance_type = "m4.xlarge"
-    mongo_blue_min_size = 0
-    mongo_blue_max_size = 0
-    mongo_blue_desired_capacity = 0
+    mongo_blue_min_size = 1
+    mongo_blue_max_size = 3
+    mongo_blue_desired_capacity = 1
 
     mongo_green_name_prefix = "${var.cluster_name}-dbsilo3-mongo-green-v1"
     mongo_green_ami_id = "ami-8537f0e5"
@@ -34,9 +34,9 @@ module "dbsilo3" {
     redis_blue_name_prefix = "${var.cluster_name}-dbsilo3-redis-blue-v2"
     redis_blue_ami_id = "ami-722dea12"
     redis_blue_instance_type = "r3.2xlarge"
-    redis_blue_min_size = 0
-    redis_blue_max_size = 0
-    redis_blue_desired_capacity = 0
+    redis_blue_min_size = 1
+    redis_blue_max_size = 3
+    redis_blue_desired_capacity = 1
 
     redis_green_name_prefix = "${var.cluster_name}-dbsilo3-redis-green-v1"
     redis_green_ami_id = "ami-722dea12"
@@ -48,9 +48,9 @@ module "dbsilo3" {
     elasticsearch_blue_name_prefix = "${var.cluster_name}-dbsilo3-elasticsearch-blue-v2"
     elasticsearch_blue_ami_id = "ami-872ee9e7"
     elasticsearch_blue_instance_type = "m3.xlarge"
-    elasticsearch_blue_min_size = 0
-    elasticsearch_blue_max_size = 0
-    elasticsearch_blue_desired_capacity = 0
+    elasticsearch_blue_min_size = 3
+    elasticsearch_blue_max_size = 3
+    elasticsearch_blue_desired_capacity = 3
 
     elasticsearch_green_name_prefix = "${var.cluster_name}-dbsilo3-elasticsearch-green-v1"
     elasticsearch_green_ami_id = "ami-872ee9e7"
