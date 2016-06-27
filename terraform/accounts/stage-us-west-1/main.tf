c2e3354a3a87acd9771c9fdb65c1b602cb5e7315
Prithviraj Pandian <prithvi.pandian@gmail.com>
2016-06-27 11:57:34 -0700

    New state file

--- terraform/accounts/stage-us-west-1/main.tf
+++ terraform/accounts/stage-us-west-1/main.tf
@@ -133,48 +133,48 @@ module "dbsilo4" {
     cluster_name = "${var.cluster_name}"
     datadog_api_key = "${var.datadog_api_key}"
 
-    mongo_blue_name_prefix = "${var.cluster_name}-dbsilo4-mongo-blue"
+    mongo_blue_name_prefix = "${var.cluster_name}-dbsilo4-mongo-blue-v2"
     mongo_blue_ami_id = "ami-a699ddc6"
     mongo_blue_instance_type = "m4.xlarge"
     mongo_blue_min_size = 1
     mongo_blue_max_size = 3
     mongo_blue_desired_capacity = 3
 
-    mongo_green_name_prefix = "${var.cluster_name}-dbsilo4-mongo-green"
+    mongo_green_name_prefix = "${var.cluster_name}-dbsilo4-mongo-green-v2"
     mongo_green_ami_id = "ami-a699ddc6"
     mongo_green_instance_type = "m4.xlarge"
-    mongo_green_min_size = 1
-    mongo_green_max_size = 3
-    mongo_green_desired_capacity = 3
+    mongo_green_min_size = 0
+    mongo_green_max_size = 0
+    mongo_green_desired_capacity = 0
 
-    redis_blue_name_prefix = "${var.cluster_name}-dbsilo4-redis-blue"
+    redis_blue_name_prefix = "${var.cluster_name}-dbsilo4-redis-blue-v2"
     redis_blue_ami_id = "ami-179eda77"
     redis_blue_instance_type = "r3.2xlarge"
     redis_blue_min_size = 1
     redis_blue_max_size = 3
     redis_blue_desired_capacity = 3
-    redis_blue_backup_file = "s3://xplain-alpha/redis-backups/${var.cluster_name}-dump.rdb"
+    redis_blue_backup_file = ""
 
-    redis_green_name_prefix = "${var.cluster_name}-dbsilo4-redis-green"
+    redis_green_name_prefix = "${var.cluster_name}-dbsilo4-redis-green-v2"
     redis_green_ami_id = "ami-179eda77"
     redis_green_instance_type = "r3.2xlarge"
-    redis_green_min_size = 1
-    redis_green_max_size = 3
-    redis_green_desired_capacity = 3
+    redis_green_min_size = 0
+    redis_green_max_size = 0
+    redis_green_desired_capacity = 0
     redis_green_backup_file = ""
 
     # WARNING: please update version number if ami changes
-    elasticsearch_blue_name_prefix = "${var.cluster_name}-dbsilo4-elasticsearch-blue-v2"
+    elasticsearch_blue_name_prefix = "${var.cluster_name}-dbsilo4-elasticsearch-blue-v3"
     elasticsearch_blue_ami_id = "ami-01a7e361"
     elasticsearch_blue_instance_type = "m3.xlarge"
     elasticsearch_blue_min_size = 3
     elasticsearch_blue_max_size = 3
     elasticsearch_blue_desired_capacity = 3
 
-    elasticsearch_green_name_prefix = "${var.cluster_name}-dbsilo4-elasticsearch-green"
-    elasticsearch_green_ami_id = "ami-bb9bdfdb"
+    elasticsearch_green_name_prefix = "${var.cluster_name}-dbsilo4-elasticsearch-green-v2"
+    elasticsearch_green_ami_id = "ami-01a7e361"
     elasticsearch_green_instance_type = "m3.xlarge"
-    elasticsearch_green_min_size = 3
-    elasticsearch_green_max_size = 3
-    elasticsearch_green_desired_capacity = 3
+    elasticsearch_green_min_size = 0
+    elasticsearch_green_max_size = 0
+    elasticsearch_green_desired_capacity = 0
 }
