12342aad6f672a728aa39b2c149eaa1480036fd1
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-06-29 18:39:17 -0700

    Rebuild redis ami for dbsilo4

--- terraform/accounts/stage-us-west-1/main.tf
+++ terraform/accounts/stage-us-west-1/main.tf
@@ -155,12 +155,12 @@ module "dbsilo4" {
     redis_blue_desired_capacity = 3
     redis_blue_backup_file = ""
 
-    redis_green_name_prefix = "${var.cluster_name}-dbsilo4-redis-green-v2"
-    redis_green_ami_id = "ami-179eda77"
+    redis_green_name_prefix = "${var.cluster_name}-dbsilo4-redis-green-v3"
+    redis_green_ami_id = "ami-8b3671eb"
     redis_green_instance_type = "r3.2xlarge"
-    redis_green_min_size = 0
-    redis_green_max_size = 0
-    redis_green_desired_capacity = 0
+    redis_green_min_size = 1
+    redis_green_max_size = 3
+    redis_green_desired_capacity = 1
     redis_green_backup_file = ""
 
     # WARNING: please update version number if ami changes
