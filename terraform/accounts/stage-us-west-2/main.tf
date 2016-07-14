05dd75d75df92133236c9a8e23c16741e2970c82
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-07-14 12:06:26 -0700

    Initial networking setup for stage-us-west-2

---
+++ terraform/accounts/stage-us-west-2/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    profile = "navopt_stage"
+    region = "${var.region}"
+}
+
+module "networking" {
+    source = "../../services/networking"
+
+    region = "${var.region}"
+    availability_zones = "${var.availability_zones}"
+
+    vpc_name = "${var.vpc_name}"
+    vpc_cidr = "${var.vpc_cidr}"
+
+    public_subnets = "${var.public_subnets}"
+    private_subnets = "${var.private_subnets}"
+
+    virtual_gateway_id = "${var.virtual_gateway_id}"
+}
---
+++ terraform/accounts/stage-us-west-2/variables.tf
@@ -0,0 +1,51 @@
+variable "account_id" {
+    default = "001209911431"
+}
+
+variable "region" {
+    default = "us-west-2"
+}
+
+variable "vpc_name" {
+    default = "navopt-stage"
+}
+
+variable "vpc_cidr" {
+   default = "10.7.0.0/17"
+}
+
+variable "availability_zones" {
+    default = "us-west-2a,us-west-2b,us-west-2c"
+}
+
+variable "public_subnets" {
+    default = "10.7.0.0/21,10.7.8.0/21,10.7.16.0/21"
+}
+
+variable "public_subnets_count" {
+    default = 3
+}
+
+variable "private_subnets" {
+    default = "10.7.24.0/21,10.7.32.0/21,10.7.40.0/21"
+}
+
+variable "private_subnets_count" {
+    default = 3
+}
+
+variable "virtual_gateway_id" {
+    default = "vgw-b4e63faa"
+}
+
+variable "cluster_name" {
+    default = "stage"
+}
+
+variable "datadog_api_key" {
+    default = "450d437e4234a90b04a15396bd2a83b8"
+}
+
+variable "key_name" {
+    default = "Baaz-Deployment"
+}
