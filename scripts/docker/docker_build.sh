4036614ad692a6736910ec126960e60c7392fe53
Samir Pujari <samir@cloudera.com>
2016-08-22 13:32:21 -0700

    Updating docker build script and docker files. Docker build file is now parameterized.

--- scripts/docker/docker_build.sh
+++ scripts/docker/docker_build.sh
@@ -1,42 +1,90 @@
 #!/bin/bash
 
-mkdir /Users/samir/git/docker/packages
-mkdir /Users/samir/git/docker/packages/pyservices
-mkdir /Users/samir/git/docker/packages/javaservices
-cd /Users/samir/git/compiler
+echo "Starting build process..."
+
+BRANCH_NAME="master"
+S3Bucket='baaz-deployment'
+if [ -z "$1" ]
+then
+  echo "No branch supplied, using master"
+else
+  BRANCH_NAME=$1
+  S3Bucket="baaz-deployment/$BRANCH_NAME"
+fi
+
+CUR_LOC=$(pwd)
+if cd $CUR_LOC/build; then echo "build folder exists"; else mkdir $CUR_LOC/build; fi
+
+#pulling all repos
+array=("deployment" "analytics" "compiler" "UI" "graph" "documentation")
+for REPO in "${array[@]}"
+do
+    :
+    echo $REPO
+    if cd $CUR_LOC/build/$REPO
+    then
+        git pull
+        git checkout $BRANCH_NAME
+        git reset --hard
+        git pull
+    else git clone https://github.com/baazdata/$REPO.git --branch $BRANCH_NAME --single-branch
+    fi
+done
+
+rm -r $CUR_LOC/docker
+cp -r $CUR_LOC/build/deployment/scripts/docker $CUR_LOC/.
+
+#Build and tar the compiler
+cd $CUR_LOC/build/compiler
+mvn clean
+mvn package -DskipTests
+if [ $? -eq 0 ]
+then
+  echo "Compiler is built"
+else
+  echo "ERROR with compiler build process"
+  exit 1
+fi
 mkdir baaz_compiler
 rm -rf baaz_compiler/*
-#mv bin/com Baaz-Hive-Compiler/.
 cp target/Baaz-Compiler/*.jar baaz_compiler/
 cp target/classes/logback.xml baaz_compiler/
 tar -cvf Baaz-Compiler.tar baaz_compiler
-gzip Baaz-Compiler.tar
-cp Baaz-Compiler.tar.gz /Users/samir/git/docker/packages/javaservices
+gzip -f Baaz-Compiler.tar
+cp Baaz-Compiler.tar.gz $CUR_LOC/docker/javaservices
 
-cd /Users/samir/git/graph
+#Build and Package all the rest of the backoffice.
+cd $CUR_LOC/build/graph
 python setup.py bdist 
 cd dist
-cp flightpath-*.tar.gz /Users/samir/git/docker/packages/pyservices/flightpath-deployment.tar.gz
+cp flightpath-*.tar.gz $CUR_LOC/docker/pyservices/flightpath-deployment.tar.gz
 echo "graph is built"
 
-cd /Users/samir/git/analytics
+cd $CUR_LOC/build/analytics
 python setup.py bdist 
 cd dist
-cp baazmath-*.tar.gz /Users/samir/git/docker/packages/pyservices/Baaz-Analytics.tar.gz
+cp baazmath-*.tar.gz $CUR_LOC/docker/pyservices/Baaz-Analytics.tar.gz
 echo "anaytics is built"
 
-cd /Users/samir/git/deployment/Data-Aquisition
+cd $CUR_LOC/build/deployment/Data-Aquisition
 tar -cf Baaz-DataAcquisition-Service.tar etc usr
-gzip Baaz-DataAcquisition-Service.tar
-cp Baaz-DataAcquisition-Service.tar.gz /Users/samir/git/docker/packages/pyservices
+gzip -f Baaz-DataAcquisition-Service.tar
+cp Baaz-DataAcquisition-Service.tar.gz $CUR_LOC/docker/pyservices
 
-cd /Users/samir/git/deployment/Compile-Server
+cd $CUR_LOC/build/deployment/Compile-Server
 tar -cf Baaz-Compile-Service.tar etc usr
-gzip Baaz-Compile-Service.tar 
-cp Baaz-Compile-Service.tar.gz /Users/samir/git/docker/packages/pyservices
+gzip -f Baaz-Compile-Service.tar 
+cp Baaz-Compile-Service.tar.gz $CUR_LOC/docker/pyservices
 
-cd /Users/samir/git/deployment/Math-Server
+cd $CUR_LOC/build/deployment/Math-Server
 tar -cf Baaz-Analytics-Service.tar etc usr
-gzip Baaz-Analytics-Service.tar 
-cp Baaz-Analytics-Service.tar.gz /Users/samir/git/docker/packages/pyservices
+gzip -f Baaz-Analytics-Service.tar 
+cp Baaz-Analytics-Service.tar.gz $CUR_LOC/docker/pyservices
+
+cd $CUR_LOC/docker/pyservices
+docker build -t navopt/pyservice .
+
+cd $CUR_LOC/docker/javaservices
+docker build -t navopt/combinedservice .
+
 echo "deployment is built"
