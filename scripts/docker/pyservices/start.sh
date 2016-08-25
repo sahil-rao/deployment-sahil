684e83cfcfd553be12ac64a63046bb59505ae89f
Samir Pujari <samir@cloudera.com>
2016-08-25 02:11:19 -0700

    Making sure start.sh script runs and compiler port is passed into the sudo call for container

--- scripts/docker/pyservices/start.sh
+++ scripts/docker/pyservices/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#start.sh
 
 j2 /hosts.j2 /config.json > /var/Baaz/hosts.cfg
 
@@ -8,21 +7,21 @@ then
 j2 /logback.j2 /config.json > /usr/lib/baaz_compiler/logback.xml
 export COMPILER_PORT=12121
 export HIVE_HOME=/usr/local/hive
-/usr/local/bin/xplaincompileserver & sudo python /usr/local/bin/ApplicationService.py
+/usr/local/bin/xplaincompileserver & sudo COMPILER_PORT=$COMPILER_PORT python /usr/local/bin/ApplicationService.py
 
 elif [ "$SERVICE_NAME" == "compileservice" ]
 then
 j2 /logback.j2 /config.json > /usr/lib/baaz_compiler/logback.xml
 export COMPILER_PORT=13131
 export HIVE_HOME=/usr/local/hive
-bash /usr/local/bin/xplaincompileserver & sudo python /usr/local/bin/BaazCompileService.py
+/usr/local/bin/xplaincompileserver & sudo COMPILER_PORT=$COMPILER_PORT python /usr/local/bin/BaazCompileService.py
 
 elif [ "$SERVICE_NAME" == "advanalytics" ]
 then
 j2 /logback.j2 /config.json > /usr/lib/baaz_compiler/logback.xml
 export COMPILER_PORT=14141
 export HIVE_HOME=/usr/local/hive
-/usr/local/bin/xplaincompileserver & sudo python /usr/local/bin/XplainAdvAnalyticsService.py
+/usr/local/bin/xplaincompileserver & sudo COMPILER_PORT=$COMPILER_PORT python /usr/local/bin/XplainAdvAnalyticsService.py
 
 elif [ "$SERVICE_NAME" == "mathservice" ]
 then
@@ -46,3 +45,4 @@ sudo python /usr/local/bin/FPProcessingService.py
 
 else
 echo "No service was found for the inputted service"
+fi
