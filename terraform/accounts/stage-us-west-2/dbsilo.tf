bfe3e9176192372f8f92c0fdf2bfbaff25368c5e
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-08-31 20:07:16 -0700

    terraform: Re-spin dbsilo

--- terraform/accounts/stage-us-west-2/dbsilo.tf
+++ terraform/accounts/stage-us-west-2/dbsilo.tf
@@ -16,24 +16,24 @@ module "dbsilo1" {
     mongo_iam_instance_profile = "${module.iam.mongo_instance_profile}"
     mongo_security_groups = "${module.sg.mongo_security_groups}"
     mongo_version = "v18"
-    mongo_ami_id = "ami-7c50981c"
+    mongo_ami_id = "ami-201fca40"
     #mongo_instance_type = "m4.xlarge"
     mongo_instance_type = "t2.micro"
-    mongo_min_size = 0
-    mongo_max_size = 0
-    mongo_desired_capacity = 0
+    mongo_min_size = 3
+    mongo_max_size = 3
+    mongo_desired_capacity = 3
     mongo_ebs_optimized = false
 
     redis_name = "${var.cluster_name}-dbsilo1-redis"
     redis_iam_instance_profile = "${module.iam.redis_instance_profile}"
     redis_security_groups = "${module.sg.redis_security_groups}"
-    redis_version = "v13"
-    redis_ami_id = "ami-f30fbcf"
+    redis_version = "v14"
+    redis_ami_id = "ami-1d57827d"
     #redis_instance_type = "r3.2xlarge"
     redis_instance_type = "t2.micro"
-    redis_min_size = 0
-    redis_max_size = 0
-    redis_desired_capacity = 0
+    redis_min_size = 3
+    redis_max_size = 3
+    redis_desired_capacity = 3
     redis_ebs_optimized = false
     redis_quorum_size = 2
 
@@ -41,11 +41,11 @@ module "dbsilo1" {
     elasticsearch_iam_instance_profile = "${module.iam.elasticsearch_instance_profile}"
     elasticsearch_security_groups = "${module.sg.elasticsearch_security_groups}"
     elasticsearch_version = "v9"
-    elasticsearch_ami_id = "ami-63fb3003"
+    elasticsearch_ami_id = "ami-8419cce4"
     #elasticsearch_instance_type = "m3.xlarge"
     elasticsearch_instance_type = "t2.micro"
-    elasticsearch_min_size = 0
-    elasticsearch_max_size = 0
-    elasticsearch_desired_capacity = 0
+    elasticsearch_min_size = 3
+    elasticsearch_max_size = 3
+    elasticsearch_desired_capacity = 3
     elasticsearch_ebs_optimized = false
 }
