67c5a6b85d9c22e1fa17a2177152af8737da1609
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-08-31 20:10:18 -0700

    admin: Cleanup dead code

--- scripts/admin/navopt_admin/redis.py
+++ scripts/admin/navopt_admin/redis.py
@@ -30,40 +30,6 @@ class Redis(object):
     def __str__(self):
         return '{}:{}'.format(self.host, self.port)
 
-#    def info(self):
-#        return
-#        if not hasattr(self, '_info'):
-#            self._info = self.rconn.info()
-#
-#        return self._info
-#
-#    @property
-#    def sentinel_masters(self):
-#        if not hasattr(self, '_sentinel_masters'):
-#            self._sentinel_masters = self.rconn.sentinel_masters()
-#
-#        return self._sentinel_masters
-#
-#    @property
-#    def sentinel_info(self):
-#        return self.sentinel_masters[self._master_name]
-#
-#    @property
-#    def sentinels(self):
-#        if self.rconn is None:
-#            return set()
-#
-#        sentinels = set()
-#
-#        # Add the current host to the sentinel list.
-#        sentinels.add('{}:{}'.format(self.host, self.port))
-#
-#        sentinels.update(
-#            sentinel['name']
-#            for sentinel in self.rconn.sentinel_sentinels(self._master_name))
-#
-#        return sentinels
-
 
 class RedisSentinel(Redis):
     def __init__(self, bastion, host, port=26379):
