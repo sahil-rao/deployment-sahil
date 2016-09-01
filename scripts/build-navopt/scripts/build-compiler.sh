2da43a95b8c87ebcf2edc63c7d4e23cda3d3b463
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-08-31 19:58:06 -0700

    build: Add new build script that uses Docker to build portable tarballs

---
+++ scripts/build-navopt/scripts/build-compiler.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+BRANCH=$1
+
+git -C /gitcache/compiler archive $BRANCH | tar -x
+
+echo "--- building compiler..."
+mvn package -DskipTests
+
+echo "--- archiving compiler..."
+mkdir baaz_compiler
+
+cp target/Baaz-Compiler/*.jar baaz_compiler/
+cp target/classes/logback.xml baaz_compiler/
+tar -zcvf /target/Baaz-Compiler.tar.gz baaz_compiler
