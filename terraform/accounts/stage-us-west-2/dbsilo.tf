42441786edcdff721f39b56225bd9e9f9a49053b
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-08-31 20:05:46 -0700

    terraform: Rename dbsilo1.tf to dbsilo.tf

--- terraform/accounts/stage-us-west-2/dbsilo.tf
+++ terraform/accounts/stage-us-west-2/dbsilo.tf
@@ -15,8 +15,8 @@ module "dbsilo1" {
     mongo_name = "${var.cluster_name}-dbsilo1-mongo"
     mongo_iam_instance_profile = "${module.iam.mongo_instance_profile}"
     mongo_security_groups = "${module.sg.mongo_security_groups}"
-    mongo_version = "v10"
-    mongo_ami_id = "ami-fe965e9e"
+    mongo_version = "v18"
+    mongo_ami_id = "ami-7c50981c"
     #mongo_instance_type = "m4.xlarge"
     mongo_instance_type = "t2.micro"
     mongo_min_size = 3
@@ -27,24 +27,24 @@ module "dbsilo1" {
     redis_name = "${var.cluster_name}-dbsilo1-redis"
     redis_iam_instance_profile = "${module.iam.redis_instance_profile}"
     redis_security_groups = "${module.sg.redis_security_groups}"
-    redis_version = "v8"
-    redis_ami_id = "ami-c6d41da6"
+    redis_version = "v13"
+    redis_ami_id = "ami-af30fbcf"
     #redis_instance_type = "r3.2xlarge"
     redis_instance_type = "t2.micro"
     redis_min_size = 0
-    redis_max_size = 6
-    redis_desired_capacity = 6
+    redis_max_size = 3
+    redis_desired_capacity = 3
     redis_ebs_optimized = false
 
     elasticsearch_name = "${var.cluster_name}-dbsilo1-elasticsearch"
     elasticsearch_iam_instance_profile = "${module.iam.elasticsearch_instance_profile}"
     elasticsearch_security_groups = "${module.sg.elasticsearch_security_groups}"
-    elasticsearch_version = "v7"
-    elasticsearch_ami_id = "ami-3310d953"
+    elasticsearch_version = "v9"
+    elasticsearch_ami_id = "ami-63fb3003"
     #elasticsearch_instance_type = "m3.xlarge"
     elasticsearch_instance_type = "t2.micro"
     elasticsearch_min_size = 0
-    elasticsearch_max_size = 6
-    elasticsearch_desired_capacity = 6
+    elasticsearch_max_size = 3
+    elasticsearch_desired_capacity = 3
     elasticsearch_ebs_optimized = false
 }
