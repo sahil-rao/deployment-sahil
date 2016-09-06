df2e392a2e2ac0e1ada2fa9386300fd687bc0153
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-09-06 13:33:04 -0700

    terraform: bump ami

--- terraform/accounts/stage-us-west-2/dbsilo1.tf
+++ terraform/accounts/stage-us-west-2/dbsilo1.tf
@@ -15,8 +15,8 @@ module "dbsilo1" {
     mongo_name = "${var.cluster_name}-dbsilo1-mongo"
     mongo_iam_instance_profile = "${module.iam.mongo_instance_profile}"
     mongo_security_groups = "${module.sg.mongo_security_groups}"
-    mongo_version = "v2"
-    mongo_ami_id = "ami-19804e79"
+    mongo_version = "v4"
+    mongo_ami_id = "ami-68a66808"
     #mongo_instance_type = "m4.xlarge"
     mongo_instance_type = "t2.micro"
     mongo_min_size = 0
@@ -27,8 +27,8 @@ module "dbsilo1" {
     redis_name = "${var.cluster_name}-dbsilo1-redis"
     redis_iam_instance_profile = "${module.iam.redis_instance_profile}"
     redis_security_groups = "${module.sg.redis_security_groups}"
-    redis_version = "v2"
-    redis_ami_id = "ami-719d5311"
+    redis_version = "v4"
+    redis_ami_id = "ami-d0ad63b0"
     #redis_instance_type = "r3.2xlarge"
     redis_instance_type = "t2.micro"
     redis_min_size = 0
@@ -39,8 +39,8 @@ module "dbsilo1" {
     elasticsearch_name = "${var.cluster_name}-dbsilo1-elasticsearch"
     elasticsearch_iam_instance_profile = "${module.iam.elasticsearch_instance_profile}"
     elasticsearch_security_groups = "${module.sg.elasticsearch_security_groups}"
-    elasticsearch_version = "v2"
-    elasticsearch_ami_id = "ami-2f9d534f"
+    elasticsearch_version = "v4"
+    elasticsearch_ami_id = "ami-7eab651e"
     #elasticsearch_instance_type = "m3.xlarge"
     elasticsearch_instance_type = "t2.micro"
     elasticsearch_min_size = 0
