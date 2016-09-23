62e1b96dcce649fab0b083372f13666fb38f629e
Samir Pujari <samir@cloudera.com>
2016-09-23 15:10:13 -0700

    Getting the jars from the correct place and enabling logging of compiler

--- scripts/update-vm.sh
+++ scripts/update-vm.sh
@@ -136,7 +136,7 @@ fi
 mkdir baaz_compiler
 rm -rf baaz_compiler/*
 #mv bin/com Baaz-Hive-Compiler/.
-cp target/Baaz-Compiler/*.jar baaz_compiler/
+cp target/Baaz-Compiler-1/*.jar baaz_compiler/
 cp target/classes/logback.xml baaz_compiler/
 tar -cvf  Baaz-Compiler.tar baaz_compiler
 gzip -f  Baaz-Compiler.tar
