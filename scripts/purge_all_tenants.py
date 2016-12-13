3982d294fc66f85c24f58341ac1b13796b4e6d3e
Gaurav Shellikeri <gaurav@cloudera.com>
2016-12-13 09:21:32 -0800

    NAVOPT-2455: Remove UI flags to DeleteTenant opcode

--- scripts/purge_all_tenants.py
+++ scripts/purge_all_tenants.py
@@ -38,13 +38,9 @@ def execute(tenantid, msg_dict=None):
         result = bucket.delete_keys([key.name for key in bucketListResultSet])
 
     """
-    - Redis: Remove Tenant routing entry
     - Redis: Remove Tenant Data
     """
-    if msg_dict and 'purgeTenant' in msg_dict:
-        deleteTenant(tenantid, purgeTenant=msg_dict['purgeTenant'])
-    else:
-        deleteTenant(tenantid)
+    deleteTenant(tenantid)
 
 
     """
@@ -68,34 +64,13 @@ def execute(tenantid, msg_dict=None):
     client = getMongoServer(tenantid)
     tenantdb = getMongoServer(tenantid)[tenantid]
     xplaindb = getMongoServer('xplainIO')['xplainIO']
-    if msg_dict and 'purgeTenant' in msg_dict and msg_dict['purgeTenant']:
-        logging.info("Going to purge the tenant")
-        client.drop_database(tenantid)
-        xplaindb.users.remove({"organizations": tenantid})
-        xplaindb.organizations.remove({"guid": tenantid})
-        return {"status": "success"}
-
-    collection_list = tenantdb.collection_names()
-    for collection in collection_list:
-        if collection == 'system.indexes' or collection == 'data_files':
-            continue
-        if collection == 'uploadStats':
-            tenantdb.uploadStats.update({'active': True},
-                                        {'$set': {'active': False}},
-                                        multi=True)
-            tenantdb.uploadStats.update({'uid': "0"},
-                                        {'$set': {'query_processed': 0,
-                                                  'limit_reached': "False"}})
-            continue
-        tenantdb.drop_collection(collection)
-
-    xplaindb.organizations.update({'guid': tenantid},
-                                  {'$set': {'queries': 0, 'lastTimeStamp': 0}})
-    xplaindb.activeUploadsUIDs.remove({'guid': tenantid})
-
+    logging.info("Going to purge the tenant")
+    client.drop_database(tenantid)
+    xplaindb.users.remove({"organizations": tenantid})
+    xplaindb.organizations.remove({"guid": tenantid})
     return {"status": "success"}
 
 if __name__ == '__main__':
     tenants_to_delete = []
     for tenantid in tenants_to_delete:
-        execute(tenantid, {'purgeTenant': True})
+        execute(tenantid)
