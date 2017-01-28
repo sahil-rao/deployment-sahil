3048e69ecda38d6b274712d8a007c2585d4232b1
Samir Pujari <samir@cloudera.com>
2017-01-27 16:08:56 -0800

    Making sure everyone has the right gulp-config

---
+++ scripts/vm-gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  workDir: "/home/xplain/gulpwork",
+  gitUrls: {
+    cui: "http://github.mtv.cloudera.com/Starship/cloudera-ui.git",
+    ui: "https://github.com/baazdata/UI.git"
+  },
+  outputDir: "/home/xplain",
+  vmDir: "/var/xplain3000/"
+}
