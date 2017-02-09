b2090443027499ebf1fa47dc88f6ded8c8866211
harshil <harshil@xplain.io>
2017-02-09 14:37:06 -0800

    Packer Cloudwatch setup script

---
+++ packer/scripts/setup_cloudwatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euv
+
+# Install awslogs
+sudo apt-get update
+sudo apt-get -y install python-pip
+sudo pip install awscli
+sudo pip install awscli-cwlogs
+cp /tmp/etc/cloudwatch/* /home/ubuntu
+sudo python /home/ubuntu/awslogs-agent-setup.py --region us-west-2 -c /home/ubuntu/awslogs.conf -n
+sudo service awslogs stop
+sudo service awslogs start
