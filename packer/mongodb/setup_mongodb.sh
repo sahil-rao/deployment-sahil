0a532e89b333458ce4379ed3d54a4bf13dc10430
Gaurav Shellikeri <gaurav@cloudera.com>
2017-02-09 16:17:43 -0800

    Upgrade mongodb ami to use Mongo 3.4

--- packer/mongodb/setup_mongodb.sh
+++ packer/mongodb/setup_mongodb.sh
@@ -2,11 +2,11 @@
 
 set -euv
 
-# Install MongoDB 3.2.11; Pin packages
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+# Install MongoDB 3.4.2; Pin packages
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
-apt-get install -y mongodb-org=3.2.11 mongodb-org-server=3.2.11 mongodb-org-shell=3.2.11 mongodb-org-mongos=3.2.11 mongodb-org-tools=3.2.11
+apt-get install -y mongodb-org=3.4.2 mongodb-org-server=3.4.2 mongodb-org-shell=3.4.2 mongodb-org-mongos=3.4.2 mongodb-org-tools=3.4.2
 echo "mongodb-org hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
 echo "mongodb-org-shell hold" | sudo dpkg --set-selections
