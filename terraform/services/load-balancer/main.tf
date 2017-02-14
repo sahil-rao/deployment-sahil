e80f34821772479159530ef6653aeb0a2941c552
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2017-02-14 14:45:35 -0800

    Clone nginx to simplify upgrading nginx in prod
    The old nginx code is tightly intertwined in the common module, which
    makes it difficult to get it into production. As a stopgap, this just
    extracts out the code into a new module.
    
    Note this uses a different AWS name so we can distinguish it from
    the real production nginx server in ansible.
    
    Note this puts the website up on www2 to test out it works before
    we transition it to be the official load balancer.

---
+++ terraform/services/load-balancer/main.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "ubuntu_ami" {
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["*ubuntu-trusty-14.04-amd64-server-*"]
+    }
+    owners = ["099720109477"]
+}
+
+resource "aws_instance" "default" {
+    ami = "${data.aws_ami.ubuntu_ami.id}"
+    vpc_security_group_ids = ["${aws_security_group.nginx.id}"]
+    subnet_id = "${element(var.subnet_ids, count.index)}"
+    key_name = "${var.key_name}"
+
+    iam_instance_profile = "${aws_iam_instance_profile.nginx.name}"
+
+    # FIXME: This is what's used in production.
+    # instance_type = "m4.xlarge"
+    instance_type = "${var.instance_type}"
+    ebs_optimized = "${var.ebs_optimized}"
+
+    tags {
+        Cluster = "${var.env}"
+        Environment = "${var.env}"
+        Name = "${var.name}"
+    }
+}
+
+output "instance_id" {
+    value = "${aws_instance.default.id}"
+}
