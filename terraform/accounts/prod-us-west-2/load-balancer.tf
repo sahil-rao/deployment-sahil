235bc204040864cb202ab703e7a6bd6491211e02
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2017-02-15 17:42:14 -0800

    terraform: import the prod frontend and admin EIPs into terraform

--- terraform/accounts/prod-us-west-2/load-balancer.tf
+++ terraform/accounts/prod-us-west-2/load-balancer.tf
@@ -22,18 +22,34 @@ module "load-balancer" {
     key_name = "${var.key_name}"
 }
 
-resource "aws_eip" "default" {
-  instance = "${module.load-balancer.instance_id}"
+resource "aws_eip" "frontend" {
+  instance = "i-c79fb81d"
   vpc      = true
 }
 
-resource "aws_route53_record" "api" {
+resource "aws_route53_record" "frontend" {
     count = "2"
     zone_id = "${element(list(data.terraform_remote_state.networking.private_zone_id,
                               data.terraform_remote_state.networking.public_zone_id), count.index)}"
-    name = "www2"
+    name = "${data.terraform_remote_state.networking.zone_name}"
     type = "A"
     ttl = "300"
 
-    records = ["${aws_eip.default.private_ip}"]
+    records = ["${aws_eip.frontend.public_ip}"]
+}
+
+resource "aws_eip" "admin" {
+  instance = "i-73fb34ae"
+  vpc      = true
+}
+
+resource "aws_route53_record" "admin" {
+    count = "2"
+    zone_id = "${element(list(data.terraform_remote_state.networking.private_zone_id,
+                              data.terraform_remote_state.networking.public_zone_id), count.index)}"
+    name = "app-dashboard"
+    type = "A"
+    ttl = "300"
+
+    records = ["${aws_eip.admin.public_ip}"]
 }
