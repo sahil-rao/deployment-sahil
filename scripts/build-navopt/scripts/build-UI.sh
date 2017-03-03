303e8102b36dcf3b766cd42d7a488d50152829c3
dmitri <dmitri@xplain.io>
2017-03-02 17:45:18 -0800

    Testing Versions

--- scripts/build-navopt/scripts/build-UI.sh
+++ scripts/build-navopt/scripts/build-UI.sh
@@ -13,6 +13,9 @@ cp deployment/scripts/gulpfile.js ./
 cp deployment/scripts/package.json ./
 ln -s /usr/bin/nodejs /usr/bin/node
 ln -s /node_modules ./node_modules
+npm -v
+node -v
+git --version
 npm install
 gulp app-build
 
