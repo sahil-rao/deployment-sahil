e3b75c4912a5d76aa082cd6ea825b008f889add4
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2017-04-19 18:13:22 -0700

    terraform: upgrade elasticsearch dbsilo 2 and 3

--- terraform/accounts/prod-us-west-2/dbsilo-elasticsearch.tf
+++ terraform/accounts/prod-us-west-2/dbsilo-elasticsearch.tf
@@ -30,3 +30,69 @@ module "dbsilo1-elasticsearch" {
 
     elasticsearch_heap_size = "8g"
 }
+
+module "dbsilo2-elasticsearch" {
+    source = "../../services/elasticsearch-asg"
+
+    vpc_id = "${data.terraform_remote_state.networking.vpc_id}"
+    subnet_ids = ["${data.terraform_remote_state.networking.private_subnet_ids}"]
+    private_cidrs = [
+        "${data.terraform_remote_state.networking.vpc_cidr}",
+        "${var.old_vpc_cidr}",
+    ]
+    zone_id = "${data.terraform_remote_state.networking.private_zone_id}"
+    zone_name = "${data.terraform_remote_state.networking.zone_name}"
+
+    key_name = "${var.key_name}"
+
+    service = "dbsilo2-elasticsearch"
+    env = "${var.cluster_name}"
+    datadog_api_key = "${var.datadog_api_key}"
+
+    name = "${var.cluster_name}-dbsilo2-elasticsearch"
+    version = "v034"
+    ami_id = "ami-b258c5d2" # elasticsearch 2.4.4
+    instance_type = "m3.xlarge"
+    min_size = 0
+    max_size = 3
+    desired_capacity = 3
+    ebs_optimized = true
+
+    cloudwatch_retention_in_days = "${var.cloudwatch_retention_in_days}"
+    log_subscription_destination_arn = "${module.log-service.destination_arn}"
+
+    elasticsearch_heap_size = "8g"
+}
+
+module "dbsilo3-elasticsearch" {
+    source = "../../services/elasticsearch-asg"
+
+    vpc_id = "${data.terraform_remote_state.networking.vpc_id}"
+    subnet_ids = ["${data.terraform_remote_state.networking.private_subnet_ids}"]
+    private_cidrs = [
+        "${data.terraform_remote_state.networking.vpc_cidr}",
+        "${var.old_vpc_cidr}",
+    ]
+    zone_id = "${data.terraform_remote_state.networking.private_zone_id}"
+    zone_name = "${data.terraform_remote_state.networking.zone_name}"
+
+    key_name = "${var.key_name}"
+
+    service = "dbsilo3-elasticsearch"
+    env = "${var.cluster_name}"
+    datadog_api_key = "${var.datadog_api_key}"
+
+    name = "${var.cluster_name}-dbsilo3-elasticsearch"
+    version = "v034"
+    ami_id = "ami-b258c5d2" # elasticsearch 2.4.4
+    instance_type = "m3.xlarge"
+    min_size = 0
+    max_size = 3
+    desired_capacity = 3
+    ebs_optimized = true
+
+    cloudwatch_retention_in_days = "${var.cloudwatch_retention_in_days}"
+    log_subscription_destination_arn = "${module.log-service.destination_arn}"
+
+    elasticsearch_heap_size = "8g"
+}
