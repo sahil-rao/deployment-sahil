214b821cf592aa4a0080a3b6d5f5c78ef4ab54f0
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2017-05-25 12:15:30 -0700

    terraform: Break up redis into multiple modules

--- terraform/services/dbsilo/main.tf
+++ terraform/services/dbsilo/main.tf
@@ -28,37 +28,6 @@ module "mongodb" {
     log_subscription_destination_arn = "${var.log_subscription_destination_arn}"
 }
 
-module "redis" {
-    source = "../redis-asg"
-
-    vpc_id = "${var.vpc_id}"
-    private_cidrs = ["${var.private_cidrs}"]
-    subnet_ids = ["${var.subnet_ids}"]
-    zone_id = "${var.zone_id}"
-    zone_name = "${var.zone_name}"
-
-    key_name = "${var.key_name}"
-
-    name = "${var.redis_name}"
-    version = "${var.redis_version}"
-    env = "${var.cluster_name}"
-    service = "${var.dbsilo_name}-redis"
-    datadog_api_key = "${var.datadog_api_key}"
-    backup_file = "${var.redis_backup_file}"
-    quorum_size = "${var.redis_quorum_size}"
-
-    ami_id = "${var.redis_ami_id}"
-    instance_type = "${var.redis_instance_type}"
-    min_size = "${var.redis_min_size}"
-    max_size = "${var.redis_max_size}"
-    desired_capacity = "${var.redis_desired_capacity}"
-    ebs_optimized = "${var.redis_ebs_optimized}"
-    backups_enabled = "${var.redis_backups_enabled}"
-
-    cloudwatch_retention_in_days = "${var.cloudwatch_retention_in_days}"
-    log_subscription_destination_arn = "${var.log_subscription_destination_arn}"
-}
-
 module "elasticsearch" {
     source = "../elasticsearch-asg"
 
