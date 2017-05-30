d876bc6eaf6137071c46b6659a7739ac86968c62
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2017-05-30 10:24:10 -0700

    packer: bump the redis local storage, and disable aof

--- packer/redis-3.2.8/build-prod.sh
+++ packer/redis-3.2.8/build-prod.sh
@@ -7,7 +7,7 @@ export AWS_PROFILE=navopt_prod
 packer build \
 	-var 'env=prod' \
 	-var 'encrypted=true' \
-	-var 'volume_size=60' \
+	-var 'volume_size=100' \
 	-var 'volume_type=io1' \
 	-var 'iops=1000' \
 	-var 'delete_on_termination=false' \
