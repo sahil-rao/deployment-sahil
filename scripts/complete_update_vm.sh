0de40d8632b747367f0b7072e713b15e40a37100
sahil-rao <sahil.rao@cloudera.com>
2017-07-05 15:47:34 -0700

    Update complete_update_vm.sh

--- scripts/complete_update_vm.sh
+++ scripts/complete_update_vm.sh
@@ -3,8 +3,8 @@ cd ~/build/deployment
 git checkout .
 git checkout master
 git pull
-cp scripts/update-vm.sh ~/.
-cp scripts/gulpfile.js ~/.
-cp scripts/package.json ~/.
+cp ./scripts/update-vm.sh ~/.
+cp ./scripts/gulpfile.js ~/.
+cp ./scripts/package.json ~/.
 cd
 ./update-vm.sh $1
